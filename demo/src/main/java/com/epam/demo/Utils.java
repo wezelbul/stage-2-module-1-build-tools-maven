d756503099f752684de85fb1416a88789b640fc8
wezelbul <d.chesnokov.01@yandex.ru>
2023-08-01 02:44:38 +0300

    feat: solution

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        if (args.size() == 0) {
+            return false;
+        }
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null || str.isEmpty() || str.isBlank() || str.charAt(0) == '0') {
+            return false;
+        }
+        try {
+            return Double.parseDouble(str) > 0;
+        } catch (NumberFormatException exception) {
+            return false;
+        }
     }
 }
